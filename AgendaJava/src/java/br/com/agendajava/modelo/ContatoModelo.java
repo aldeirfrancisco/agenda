bb7f2642a110b91c40d72810e07e071bdf8c5348
aldeirfrancisco <dide123francisco@gmail.com>
2019-08-19 23:15:01 -0300

    comentei a conexao e fiz  o modelo

---
+++ AgendaJava/src/java/br/com/agendajava/modelo/ContatoModelo.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.agendajava.modelo;
+
+/**
+ *
+ * @author aldeir
+ */
+public class ContatoModelo {
+    private Integer id;
+    private String nome;
+    private String email;
+    private String sexo;
+    private String telefone;
+    private String rua;
+    private String conplemento;
+    private int numero;
+    private String bairro;
+    private String cidade;
+   private String uf;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getConplemento() {
+        return conplemento;
+    }
+
+    public void setConplemento(String conplemento) {
+        this.conplemento = conplemento;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    
+    
+        
+    
+        
+        
+}
