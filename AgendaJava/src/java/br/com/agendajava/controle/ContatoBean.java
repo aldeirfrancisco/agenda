7fa58cd24caf2b5805fe018492dd048637f1217a
aldeirfrancisco <dide123francisco@gmail.com>
2019-08-30 16:45:56 -0300

    dando problema

--- AgendaJava/src/java/br/com/agendajava/controle/ContatoBean.java
+++ AgendaJava/src/java/br/com/agendajava/controle/ContatoBean.java
@@ -7,6 +7,8 @@ package br.com.agendajava.controle;
 
 import br.com.agendajava.dao.ContatoDao;
 import br.com.agendajava.modelo.ContatoModelo;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -14,12 +16,14 @@ import javax.faces.bean.SessionScoped;
  @ManagedBean(name = "contatobean")
  @SessionScoped
 public class ContatoBean {
-    private ContatoDao contDao = new ContatoDao();
-    private ContatoModelo contMode;
-
-    public ContatoBean (){
      
-     contMode = new ContatoModelo();
+    private ContatoDao contDao = new ContatoDao();
+    private ContatoModelo contMode = new ContatoModelo();;
+    private  List<ContatoModelo> lista = new VirtualFlow.ArrayLinkedList<>();
+    
+    public ContatoBean (){
+     buscar();
+    // contMode = new ContatoModelo();
  }
     public ContatoDao getContDao() {
         return contDao;
@@ -36,10 +40,22 @@ public class ContatoBean {
     public void setContMode(ContatoModelo contModelo) {
         this.contMode = contModelo;
     }
+
+    public List<ContatoModelo> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<ContatoModelo> lista) {
+        this.lista = lista;
+    }
+    
     public void salvarDados(){
       //if(contMode.getId() == null){
           contDao.salvar(contMode);
           contMode = new ContatoModelo();
     
 }
+    public void buscar(){
+    lista = contDao.busca();
+    }
  }
